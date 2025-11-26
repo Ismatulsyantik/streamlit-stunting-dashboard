12cca5d2196afc985474f6fdc86b553a778f1be2
Ismatulsyantik <itmaza5682@gmail.com>
2025-11-26 21:16:28 +0700

    fix API key + model format

--- components/ai_engine.py
+++ components/ai_engine.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import pandas as pd
 from .ai_prompt import CHATBOT_SYSTEM
 
-genai.configure(api_key="GOOGLE_API_KEY")
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("gemini-2.0-flash")
 def generate_chat_answer(df: pd.DataFrame, user_message: str) -> str:
     # Dataframe -> text
@@ -20,5 +20,5 @@ PERTANYAAN USER:
 JAWABAN:
     """
 
-    response = model.generate_content([prompt])
+    response = model.generate_content(prompt)
     return response.text
